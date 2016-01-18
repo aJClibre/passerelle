031695acae932f88d5d778305aff14e0718334ff
aJCLibre <a.jean-charles@valabre.com>
2016-01-18 17:02:38 +0100

    Begin tests for send data to debian server
    - complete notes.md to explain the process

--- scripts_python3/scripts/sendDataToNFrance.py
+++ scripts_python3/scripts/sendDataToNFrance.py
@@ -3,5 +3,5 @@
 import requests
 
 if __name__ == "__main__":
-    r = requests.post("http://192.168.111.191", json={"data": {"1": "data1", "2": "data2"}})
-    print( r )
+    r = requests.post("http://192.168.111.191/passerelle/script_python3/scripts/receiveDataFromSystel.py", json={"data": {"1": "data1", "2": "data2"}})
+    print( r.text )
